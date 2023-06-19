db114e131946d9ed23cdbd4fc8804d8ad88a13eb
daryamaximenko <i@dkarushina.ru>
2023-06-19 22:40:38 +0300

    fix bug with token

--- controllers/users.js
+++ controllers/users.js
@@ -85,7 +85,7 @@ module.exports.login = (req, res, next) => {
         httpOnly: true,
         sameSite: 'none',
         secure: true,
-      }).send({ message: messages.ok });
+      }).send({ token });
     })
     .catch(next);
 };
